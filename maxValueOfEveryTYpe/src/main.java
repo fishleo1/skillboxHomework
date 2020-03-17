e7559573ba65e59b4f5184aa3e10f5573ecc76ec
fishleo1 <leon018.com@gmai.com>
2020-03-17 14:05:08 +0300

    Выполнение второго домашнего задания четвертого модуля

---
+++ maxValueOfEveryTYpe/src/main.java
@@ -0,0 +1,22 @@
+public class main {
+    public static void typeValue(String[] args) {
+        System.out.println("Integer maximum value is: " + Integer.MAX_VALUE);
+        System.out.println("Integer minimum value is: " + Integer.MIN_VALUE);
+
+        System.out.println("Byte maximum value is: " + Byte.MAX_VALUE);
+        System.out.println("Byte minimum value is: " + Byte.MIN_VALUE);
+
+        System.out.println("Short maximum value is: " + Short.MAX_VALUE);
+        System.out.println("SHort minimum value is: " + Short.MIN_VALUE);
+
+        System.out.println("Long maximum value is: " + Long.MAX_VALUE);
+        System.out.println("Long minimum value is: " + Long.MIN_VALUE);
+
+        System.out.println("Float maximum value is: " + Float.MAX_VALUE);
+        System.out.println("Float minimum value is: " + Float.MIN_VALUE);
+
+        System.out.println("Double maximum value is: " + Double.MAX_VALUE);
+        System.out.println("Double minimum value is: " + Double.MIN_VALUE);
+
+    }
+}
