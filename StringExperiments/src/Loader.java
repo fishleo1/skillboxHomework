314d3216053c87386f7555c156bec6762573d384
Fish Leo <leon018.com@gmail.com>
2020-04-14 15:14:49 +0300

    calculate total salary - done

--- StringExperiments/src/Loader.java
+++ StringExperiments/src/Loader.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -9,12 +11,33 @@ public class Loader
             int c = i;
             System.out.println(i + " :" + c);
         }
+        List<String> allSalarys = new ArrayList<>(2);
         String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
-        String salaryOne = text.substring(15, 19);
-        String salaryTwo = text.substring(56, 61);
-        int salaryOne1 = Integer.parseInt(salaryOne);
-        int salaryTwo1 = Integer.parseInt(salaryTwo);
-        int result = salaryOne1 + salaryTwo1;
-        System.out.println(result);
+
+        Pattern p = Pattern.compile("\\b\\d+\\b");
+        Matcher m = p.matcher(text);
+
+        while(m.find()){
+            allSalarys.add(m.group());
+        }
+        System.out.println("Salary is: " + allSalarys.get(0) + " " + allSalarys.get(1) + " " + allSalarys.get(2));
+
+        int first = Integer.parseInt(allSalarys.get(0));
+        int sec = Integer.parseInt(allSalarys.get(1));
+        int third = Integer.parseInt(allSalarys.get(2));
+
+        int total = first + sec + third;
+        System.out.println("Total salary is: " + total);
+
+
+
+
+
+//        String salaryOne = text.substring(15, 19);
+//        String salaryTwo = text.substring(56, 61);
+//        int salaryOne1 = Integer.parseInt(salaryOne);
+//        int salaryTwo1 = Integer.parseInt(salaryTwo);
+//        int result = salaryOne1 + salaryTwo1;
+//        System.out.println(result);
     }
 }
